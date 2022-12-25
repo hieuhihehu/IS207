424a018668244f646cac70244053394f90572a2a
hellofromtheothersky <ensown2002@gmail.com>
2022-12-26 00:41:32 +0700

    intial

---
+++ public/template/footer_top_1.php
@@ -0,0 +1,40 @@
+<section class="container-fluid footer-top1">
+    <section class="container">
+      <section class="row-fluid">
+        <figure class="span3">
+          <h4>Đăng ký nhận tin</h4>
+          <p>Đăng ký nhận thông tin sách mới, sách bán chạy và khuyến mãi từ Book Garden</p>
+          <input name="" type="text" class="field-bg" value="Nhập địa chỉ email của bạn"/>
+          <input name="" type="submit" value="Đăng ký" class="sub-btn" />
+        </figure>
+        <figure class="span3">
+          <h4>Về Team</h4>
+          <ul class="tweets-list">
+            <li><a href="https://www.facebook.com/thienanh1412">Tô Diệp Thiên Anh</a></li>
+            <li><a href="https://www.facebook.com/huanguyentankhoa">Hứa Nguyễn Tân Khoa</a></li>           
+            <li><a href="#">Chính sách bảo mật</a></li>
+            <li><a href="#">Điều khoản sử dụng</a></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </figure>
+        <figure class="span3">
+          <h4>Trợ giúp</h4>
+          <ul class="tweets-list">
+              <li><a href="#">Quy định sử dụng</a></li>
+              <li><a href="#">Hướng dẫn mua hàng</a></li>
+                <li><a href="#">Phương thức thanh toán</a></li>
+                <li><a href="#">Phương thức vận chuyển</a></li>
+                <li><a href="#">Chính sách đổi trả</a></li>
+                <li><a href="#">Các câu hỏi thường gặp</a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </figure>
+        <figure class="span3">
+          <h4>Địa chỉ</h4>
+            <p>Khu phố 6, Thủ Đức, Thành phố Hồ Chí Minh</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d125392.55373495536!2d106.67950428703251!3d10.84820348678763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d10.829824!2d106.69260799999999!4m5!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zxJHhuqFpIGjhu41jIGPDtG5nIG5naOG7hyB0aMO0bmcgdGlu!3m2!1d10.8699184!2d106.8038081!5e0!3m2!1sen!2s!4v1597544017694!5m2!1sen!2s" h="600" width="300" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+            
+        </figure>
+      </section>
+    </section>
+  </section>
